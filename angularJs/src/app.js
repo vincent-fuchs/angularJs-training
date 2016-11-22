dfc1faa38d077614c46bd1c2922f422f9cfe127b
Vincent Fuchs <vincent.fuchs@gmail.com>
2016-11-22 12:32:25 +0530

    Create app.js

---
+++ angularJs/src/app.js
@@ -0,0 +1,27 @@
+angular.module('MyApp', [
+    'MyApp.controllers',
+    'MyApp.directives',
+    'MyApp.providers',
+    'MyApp.services',
+    'MyApp.filters'
+]).config([
+    function () {
+        'use strict';
+        /*your code */
+    }
+]).factory([
+    function () {
+        'use strict';
+        /*your code */
+    }
+]).run([
+    function () {
+        'use strict';
+        /*your code */
+    }
+]);
+angular.module('MyApp.controllers', []);
+angular.module('MyApp.services', []);
+angular.module('MyApp.providers', []);
+angular.module('MyApp.directives', []);
+angular.module('MyApp.filters', []);
